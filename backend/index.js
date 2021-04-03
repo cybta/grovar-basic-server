959b06445505af9bdbc8b740fd77597a00c732ee
dani@madcap.me <c.saad@live.com>
2021-04-03 12:39:24 +0300

    addig product id api

--- backend/index.js
+++ backend/index.js
@@ -16,6 +16,20 @@ app.get("/API/products", function (req, res) {
   res.send(dataProducts);
 });
 
+app.get("/API/products/:id", (req, res) => {
+  const { id } = req.params;
+  let found = false;
+  dataProducts.forEach((product) => {
+    if (product.product_id === id) {
+      found = true;
+      return res.json(product);
+    }
+  });
+  if (!found) {
+    res.status(404).json("No such product");
+  }
+});
+
 app.listen(
   process.env.PORT || 5000,
   console.log("Server is runing on port 5000")
